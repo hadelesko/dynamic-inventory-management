7cf6a54353dc1ef1aa003d7d22853c140e97b354
Koomi <koomiadeh@gmail.com>
2018-08-04 15:51:44 -0500

    1rst change

--- inventory-management/src/main/java/org/launchcode/inventorymanagement/data/Order.java
+++ inventory-management/src/main/java/org/launchcode/inventorymanagement/data/Order.java
@@ -1,5 +1,43 @@
 package org.launchcode.inventorymanagement.data;
 
+import com.sun.istack.internal.NotNull;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Order {
-    private int
+
+    @Id
+    @GeneratedValue
+    private int id;
+    @NotNull
+    private String productlinename;
+    private int schift;
+
+    public Order() {
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductlinename() {
+        return productlinename;
+    }
+
+    public void setProductlinename(String productlinename) {
+        this.productlinename = productlinename;
+    }
+
+    public int getSchift() {
+        return schift;
+    }
+
+    public void setSchift(int schift) {
+        this.schift = schift;
+    }
 }
